4c78308e20ff3c54846126d9821c9fae282a0dfd
Artur <artur@3dlook.me>
2022-01-18 21:19:24 +0200

    init project

---
+++ main.py
@@ -0,0 +1,91 @@
+# Интерпритируемый язык програмирования
+# Строгая динамическая типизация
+
+# Названия переменных, функция, классов и т п - буквы, цифры (не на первом месте), нижнее подчеркивание
+# Регистрочувствительные
+# Человекочитаемые имена
+
+# a = 12
+# b = 23
+# c = a + b
+#
+# print(c)
+
+# ctr + d
+
+# Print command
+# print('Hello world!')
+#
+# _asdasdaASDASDA33453542 = 4
+#
+# # a != A - регистрозависимы
+# value = 1
+# # print(Value)
+#
+# first = 10
+# second = 23
+# sum_of_int = first + second
+#
+# student_1_mark = 12
+# student_2_mark = 11
+#
+# average_mark = (student_1_mark + student_2_mark) / 2
+
+# int, float, comlex
+# str
+# bool
+
+# list
+# tuple
+# set
+# dict
+
+
+# int, float
+
+# first = 12
+# second = 12.0
+# print(type(first))
+# print(type(second))
+
+# c = 12 + 5 + 33 * 2 - 4.0
+# c = 10 / 5
+# c = 2 + .0
+
+# c = 0.1 + 0.2
+# c = c * 100
+# c = 2 ** 5
+# c = 0
+# c += 1  # c = c + 1
+#
+# c = '23'
+
+# var c: integer - Pascal
+# print(c)
+# print(type(c))
+#
+# c = 12 * 10
+# c = 12 / 10
+# c = 12 + 10
+# c = 12 - 10
+# c = 12 ** 10
+# c = (12 + 4) * 8
+
+# c = 4e-17  # 4 * (10 ** 17)
+# print(c)
+# print(type(c))
+
+# a = '2.3'
+# a = float(a)
+# a = float(2333)
+# a = int('3434344')
+# # a = int(a)
+# print(a)
+# print(type(a))
+
+
+# git init
+# git status
+# git add <file>
+
+# 1 'a' -> 'b'
